a8d5893d84d75c2033973a31022d6999dac1b35d
ahmednurmok <ahmednurmok@gmail.com>
2025-11-12 20:38:39 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+function handleClick(){
+   alert: ("i got clicked");
+
+}
+//Alternatively  document.querySelector('button').addEventListener('click', function(){alert: (“I got clicked”);
+
+var drumbuttons = document.querySelector('.drum').length;
+//.length counts all drumms. without it only the 1st drum would be counted  
+for (var i=0; i<drumbuttons;) {
+document.querySelector('.drum')[i].addEventListener('click', handleClick)};
+//for loop,  parameter is i instead of listing all 7 buttons.
