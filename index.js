dd8e38138b13652e678eca12342a68ea7f8654c2
ahmednurmok <ahmednurmok@gmail.com>
2025-11-12 22:42:52 +0000

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ function handleClick(){
 }
 //Alternatively  document.querySelector('button').addEventListener('click', function(){alert: (“I got clicked”);
 
-var drumbuttons = document.querySelector('.drum').length;
+var drumbuttons = document.querySelectorAll('.drum').length;
 //.length counts all drumms. without it only the 1st drum would be counted  
-for (var i=0; i<drumbuttons;) {
-document.querySelector('.drum')[i].addEventListener('click', handleClick)};
+for (var i=0; i<drumbuttons; i++) {
+document.querySelectorAll('.drum')[i].addEventListener('click', handleClick)};
 //for loop,  parameter is i instead of listing all 7 buttons.
